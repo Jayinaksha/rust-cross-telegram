7e903d95a61e3d627658bb567282eddd4059c201
jayinaksha <jayinksha_2302cm08@iitp.ac.in>
2026-03-06 16:20:43 +0530

    Android Functionality Updated.

--- android/src/lib.rs
+++ android/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use core::{render_ui, AppState};
+use eframe::egui;
+
+use winit::platform::android::activity::AndroidApp;
+use winit::platform::android::EventLoopBuilderExtAndroid;
+
+pub struct App {
+    state: AppState,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            state: AppState::default(),
+        }
     }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            render_ui(ui, &mut self.state);
+        });
+    }
+}
+
+#[unsafe(no_mangle)]
+fn android_main(app: AndroidApp) {
+    android_logger::init_once(
+        android_logger::Config::default().with_max_level(log::LevelFilter::Info),
+    );
+
+    let options = eframe::NativeOptions {
+        event_loop_builder: Some(Box::new(move |builder| {
+            builder.with_android_app(app);
+        })),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Rust Telegram App",
+        options,
+        Box::new(|_| Box::new(App::default())),
+    )
+    .expect("failed to start eframe");
 }
